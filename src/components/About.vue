06076eeff8ff1ad5417d7a501a943f98a7a6bfe9
AlvinElves <hong3842@gmail.com>
2024-01-25 21:17:59 +0800

    feat: added my experience timeline

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    About
+    <div >
+      <div 
+        class="text-3xl font-bold text-gray-800 px-2 py-2 ml-8 mr-8 border-b-2 border-black mb-2"
+      >
+        About Me
+      </div>
+    </div>
+    <div>
+      <div 
+        class="text-3xl font-bold text-gray-800 px-2 py-2 ml-8 mr-8 border-b-2 border-black mb-2"
+      >
+        Work & Voluntary Timeline
+      </div>
+      <Timeline />
+    </div>
+    <div >
+      <div 
+        class="text-3xl font-bold text-gray-800 px-2 py-2 ml-8 mr-8 border-b-2 border-black mb-2"
+      >
+        Testimonials
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Timeline from "@/components/AboutMe/Timeline"
+
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  components: {
+    Timeline
+  }
 }
 </script>
