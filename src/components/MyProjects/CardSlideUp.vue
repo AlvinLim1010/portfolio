07b8e5d49257e982fb7c31161c2d392320193d59
AlvinElves <hong3842@gmail.com>
2024-02-19 20:24:33 +0800

    feat: added personal projects slideup card

--- src/components/MyProjects/CardSlideUp.vue
+++ src/components/MyProjects/CardSlideUp.vue
@@ -3,7 +3,7 @@
     <div class="relative group h-96 overflow-hidden bg-black">
       <img
         class="object-cover w-full h-full transform duration-700 backdrop-opacity-100"
-        src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?auto=format&fit=crop&w=1650&q=80"
+        :src="getImagePath(imagePath)"
       />
       <div
         class="absolute h-full shadow-2xl opacity-20 transform duration-500 inset-y-full group-hover:-inset-y-0"
@@ -15,13 +15,12 @@
           <p
             class="capitalize font-serif font-bold text-3xl text-center shadow-2xl text-white mt-10"
           >
-            Team work
+            {{ title }}
           </p>
         </div>
         <div class="absolute w-full flex place-content-center mt-20">
           <p class="font-sans text-center w-4/5 text-white mt-5">
-            Collaborative effort of a group to achieve a common goal the most
-            effective and efficient way!
+            {{ text }}
           </p>
         </div>
       </div>
@@ -32,6 +31,16 @@
 <script>
 export default {
   name: "CardSlideUp",
+  props: {
+    imagePath: String,
+    title: String,
+    text: String,
+  },
+  methods: {
+    getImagePath(imagePath) {
+      return require(`@/assets/pictures/${imagePath}.png`);
+    },
+  }
 };
 </script>
     
--- src/components/MyProjects/ProjectCards.vue
+++ src/components/MyProjects/ProjectCards.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="mx-4 sm:ml-8 sm:mx-0">
-    <CardSlideUp />
+    <CardSlideUp 
+      :imagePath="image"
+      :title="title"
+      :text=text
+    />
     <div class="flex justify-between">
       <button
-        class="bg-gray text-black font-bold rounded-lg h-10 w-24 ml-8"
-        @click="contactUs"
+        class="bg-gray text-black bg-gray-500 font-bold rounded-lg h-12 w-32 ml-6 mt-5"
+        @click="goToCode"
       >
         Go To Code
       </button>
       <button
-        class="bg-gray text-black font-bold rounded-lg h-10 w-24 mr-8"
-        @click="contactUs"
+        class="bg-gray text-black bg-gray-500 font-bold rounded-lg h-12 w-32 mr-6 mt-5"
+        @click="videoDemo"
       >
         Video Demo
       </button>
@@ -23,8 +27,23 @@ import CardSlideUp from "@/components/MyProjects/CardSlideUp";
 
 export default {
   name: "ProjectCards",
+  props: {
+    image: String,
+    title: String,
+    text: String,
+    code: String,
+    video: String,
+  },
   components: {
     CardSlideUp,
   },
+  methods: {
+    goToCode() {
+      window.open(this.code, '_blank');
+    },
+    videoDemo() {
+      this.isProfilePictureOpen = true;
+    },
+  }
 };
 </script>
